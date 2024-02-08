76fe3c7b85744615a51095796cbca66246bce302
Checo894 <A01068505@tec.mx>
2024-02-08 10:11:31 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./Components/Navbar/Navbar";
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import ShopCategory from "./Pages/ShopCategory";
+import Inicio from "./Pages/Inicio";
+import Product from "./Pages/Product";
+import Login from "./Pages/Login";
+import Footer from "./Components/Footer/Footer";
+import Blog from "./Pages/Blog";
+import Conocenos from "./Pages/Conocenos";
+import Contacto from "./Pages/Contacto";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Inicio/>}/>
+          <Route path="/blog" element={<Blog/>}/>
+          <Route path="/pastas" element={<ShopCategory category="pastas"/>}/>
+          <Route path="/tacon" element={<ShopCategory category="tacon"/>}/>
+          <Route path="/remache" element={<ShopCategory category="remache"/>}/>
+          <Route path="/kit" element={<ShopCategory category="kit"/>}/>
+          <Route path="/conocenos" element={<Conocenos/>}/>
+          <Route path="/contacto" element={<Contacto/>}/>
+          <Route path="/product" element={<Product/>}>
+            <Route path=":productId" element={<Product/>}/>
+          </Route>
+          <Route path="/login" element={<Login/>}/>
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
     </div>
   );
 }
